e5af40f7a102e4b8b778be6ea58fdd7ee00311b5
M S Deekshith <135257768+itzme-deekshith@users.noreply.github.com>
2025-03-26 18:37:10 +0530

    Add files via upload

---
+++ Arrays/Valid Anagram.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        return countS == countT
+    
+Obj = Solution()
+s = "anagram"
+t = "nagaram"
+result = Obj.isAnagram(s,t)
+print(s,t, "is anagram --> ",result)
+
+s = "rat"
+t = "car"
+result = Obj.isAnagram(s,t)
+print(s,t, "is anagram --> ",result)
